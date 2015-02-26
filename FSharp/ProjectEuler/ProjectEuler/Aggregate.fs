857adbfc889e3e8a5003ebe42e2f9a1c00530729
Tim Butterfield <tim@timbutterfield.co.uk>
2015-02-26 22:02:25 +0000

    Started the second euler problem and refactorred function for problem 1

--- FSharp/ProjectEuler/ProjectEuler/Aggregate.fs
+++ FSharp/ProjectEuler/ProjectEuler/Aggregate.fs
@@ -1,9 +1,7 @@
 namespace ProjectEuler
 
 type Aggregate() = 
-    member this.multiplesOf (a:int, b:int) = 
-        [0 .. 999]
+    member this.multiplesOf (a:int, b:int, range) = 
+        range
         |> List.filter(fun x -> x % a = 0 || x % b = 0 )
         |> List.sumBy (fun x -> x)    
-
-    member this.func2 (x:int) = 2*x*x - x/5 + 3
